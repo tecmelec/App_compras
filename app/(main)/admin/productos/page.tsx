830ec2477c782f93d52ac03f9c7fafe32c7f3b61
Tecmelec Electricidad S.L. <albert@tecmelec.es>
2026-09-24 10:46:30 +0200

    Add files via upload

--- app/(main)/admin/productos/page.tsx
+++ app/(main)/admin/productos/page.tsx
@@ -16,7 +16,7 @@ export default async function ProductosAdminPage() {
   const { data: productos } = await supabase
     .from('productos')
     .select(
-      'id, nombre, descripcion, imagen_url, categoria, visible, precio, unidad_medida, bc_item_no, proveedor_predeterminado_id'
+      'id, nombre, descripcion, imagen_url, categoria, visible, precio, unidad_medida, bc_item_no, proveedor_predeterminado_id, multiplo_compra'
     )
     .order('categoria');
 
